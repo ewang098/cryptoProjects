b23a0368548bfae036945067df82754351e4f902
ewang331 <ewang098@utexas.edu>
2021-11-19 00:05:07 -0800

    algorand tracker

---
+++ CryptoPriceTracker/AlgoPriceTracker.py
@@ -0,0 +1,67 @@
+import time, schedule
+import config
+
+from twilio.rest import Client
+from datetime import datetime
+from pycoingecko import CoinGeckoAPI
+
+cg = CoinGeckoAPI()
+
+def getTextMessage(coin, threshold, notificationType, currency = 'usd'):
+  priceOfAlgo = (cg.get_price(ids=coin, vs_currencies=currency)[coin][currency])
+
+  textMessage = None
+  if priceOfAlgo <= threshold:
+    textMessage = notificationType \
+                + "\n" + 'Hello, the price of Algorand is ' + '$' + str(priceOfAlgo) \
+                + "\n" + config.url
+
+  return textMessage, priceOfAlgo
+
+def sendMessage(priceMessage):
+  account_sid = config.account_sid
+  auth_token = config.auth_token
+  client = Client(account_sid, auth_token)
+
+  message = client.messages \
+                  .create(
+                      body=priceMessage,
+                      from_= config.senderPhone,
+                      to= config.receiverPhone
+                  )
+
+  print(message.sid)
+
+def textLoop(threshold, notificationType):
+  priceMessage, priceOfAlgo = getTextMessage('algorand', threshold, notificationType)
+
+  now = datetime.now()
+  currentTime = now.strftime("%H:%M:%S")
+  if notificationType == 'ALERT:':
+    print('@' + currentTime + ' Price of Algo is ' + str(priceOfAlgo))
+  if priceMessage is not None:
+    sendMessage(priceMessage)
+
+def scheduleSelector(threshold, selector, notificationType, time=1):
+  if selector == 'sec':
+    schedule.every(time).seconds.do(textLoop, threshold, notificationType)
+  elif selector == 'min':
+    schedule.every(time).minutes.do(textLoop, threshold, notificationType)
+  elif selector == 'hr':
+    schedule.every(time).hours.do(textLoop, threshold, notificationType)
+
+def main():
+  #create an object
+  thresholdAlert, thresholdUpdate  = 1.60, 200.00
+  lengthAlert, lengthUpdate = 1, 1
+  selector = ['sec', 'min', 'hr']
+
+  scheduleSelector(thresholdAlert, selector[1], 'ALERT:', lengthAlert)
+  scheduleSelector(thresholdUpdate, selector[2], 'UPDATE:', lengthUpdate)
+
+  while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+if __name__ == "__main__":
+  main()
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cryptoProjects
 Hobby Crypto Projects
+
+##To Do:
+1. Cleaner code and OOP
+  - Clean up methods and Comment
+  - Create an object (priceTracker?) for each notification
+2. Incorporate user input
+  - User can choose Crypto from set list
+  - Set threshold based on current price
+    -ex: threshold = 0.9 x current price
+3. Incorporate user creation and deletion
+  - User can CRUD a price tracker
